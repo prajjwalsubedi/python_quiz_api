e0b641597fe1529e97317498db0c97ce370eeef3
Prajjwal Subedi <prajjwalsubedi95@gmail.com>
2022-11-04 21:24:21 +1100

    unescape the html in question

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,4 @@
+import html
 class QuizBrain:
 
     def __init__(self, question_list):
@@ -11,7 +12,7 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q. {self.question_number}: {current_question.text} (True or False):  ")
+        user_answer = input(f"Q. {self.question_number}: {html.unescape(current_question.text)} (True or False):  ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
